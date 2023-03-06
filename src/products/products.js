04f733654e0bf5d31536e1065c528eae74c62d7a
Felipe Alves <felipealves3580@gmail.com>
2023-03-06 12:40:04 -0300

    Add products

---
+++ src/products/products.js
@@ -0,0 +1,28 @@
+import berserk from "./img/capa-berserk-vol-1.jpg";
+import dragon from "./img/capa-dragon-ball-vol-23.jpg";
+import soulEater from "./img/capa-soul-eater-vol-1.jpg";
+
+const products = [
+  {
+    id: 1,
+    img: berserk,
+    name: "Berserk Vol.1",
+    price: 32.9,
+  },
+
+  {
+    id: 2,
+    img: dragon,
+    name: "Dragon Ball Vol.23",
+    price: 22.9,
+  },
+
+  {
+    id: 3,
+    img: soulEater,
+    name: "Soul Eater Vol.1",
+    price: 36.9,
+  },
+];
+
+export default products;
