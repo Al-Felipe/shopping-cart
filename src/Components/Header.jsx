4a24086a53cc74a218ff9a64ea2f81f0abd808f6
Felipe Alves <felipealves3580@gmail.com>
2023-03-06 09:46:50 -0300

    Create header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="logo">Mangazil</h1>
+      <nav>
+        <p>Inicio</p>
+        <p>Produtos</p>
+        <p>Contato</p>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
